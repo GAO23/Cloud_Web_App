5ef953cc70dbe56cabdd796be1d081643a1f20cb
GAO23 <gaoxiangshuai@gmail.com>
2019-12-22 04:53:37 -0500

    updated

--- cloud_drive_front_end/src/components/download.js
+++ cloud_drive_front_end/src/components/download.js
@@ -13,19 +13,19 @@ class Download extends React.Component{
 
     async download(){
         try{
-            let url = 'http://localhost:3001/pdf';
+            let url = 'http://localhost:3001/';
             const response = await axios({
                 url: url,
                 method: 'GET',
-                responseType: 'blob', // important
+                responseType: 'application/json', // important
             });
             console.log(response);
-            url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'network.pdf');
-            //document.body.appendChild(link);
-            link.click();
+            // url = window.URL.createObjectURL(new Blob([response.data]));
+            // const link = document.createElement('a');
+            // link.href = url;
+            // link.setAttribute('download', 'network.pdf');
+            // //document.body.appendChild(link);
+            // link.click();
         }catch(err){
             console.log(err.message);
         }
